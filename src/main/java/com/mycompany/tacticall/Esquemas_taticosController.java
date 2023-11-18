55e5d4773b9c943fda96b6a1cccedc3b33884f66
moond <moond@LorenzoPC>
2023-11-18 02:46:58 -0300

    Nova tela de Home e Times

---
+++ src/main/java/com/mycompany/tacticall/Esquemas_taticosController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.tacticall;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author moond
+ */
+public class Esquemas_taticosController extends Sidebar implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Espacamento_Botoes();
+        try {
+            VerificaLogin();
+        } catch (IOException ex) {
+            Logger.getLogger(TimesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }     
+    
+}
