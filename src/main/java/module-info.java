e3ffce534b9acca4033fbfb21cc890c98bbf5fcf
Luiz Guilherme de Souza <l.guilhermedesouza@gmail.com>
2023-11-19 22:04:05 -0300

    SoccerService

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module com.mycompany.tacticall {
     requires javafx.fxml;
     requires java.base;
     requires java.sql;
-
+    requires java.net.http;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
     opens com.mycompany.tacticall to javafx.fxml;
     exports com.mycompany.tacticall;
 }
